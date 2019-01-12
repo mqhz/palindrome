2bad7dcf7b120df1193cfe737ffd84217ba64ba3
QUAYAM <quayamzaidi2010@gmail.com>
2019-01-13 01:49:20 +0530

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
+
+module.exports = Phrase;
+
 //Reverse the string, But there is a catch, We are modifying the "String" object itself with reverse() method.
 String.prototype.reverse = function reverse ()
 {
 return Array.from(this).reverse().join("");
 }
 
-
 //Defines a Phrase object.
 function Phrase(content)
 {
   this.content = content;
 
-//To eleminate the use of toLowerCase() method (which in your opinion is unnecessory).
-this.processor = function processor(string)
-{
-return string.toLowerCase();
-}
+  //Provide an output of a String consist of only letters in that String.
+  this.letters = function letters()
+  {
+  return (this.content.match(/[a-z]/gi) || []).join("");
+  }
 
 //Process the content by converting it to LowerCase.
 this.processedContent = function processedContent()
 {
-return this.processor(this.content);
+return this.letters().toLowerCase();
 }
 
 //Check for palindrome.
@@ -27,28 +29,4 @@ this.palindrome = function palindrome()
 {
   return this.processedContent() === this.processedContent().reverse();
 }
-
-//Convert the supplied stirng to upper case.
-this.louder = function louder()
-{
-  let convert = this.content.toUpperCase();
-  return convert;
 }
-}
-
-
-//Defines a TranslatedPhrese object.
-function TranslatedPhrase(content, translation)
-{
-  this.content = content;
-  this.translation = translation;
-
-//Overriding the processedContent() method of Phrase object.
-this.processedContent = function processedContent()
-{
-  return this.processor(this.translation);
-}
-
-}
-
-TranslatedPhrase.prototype = new Phrase();
